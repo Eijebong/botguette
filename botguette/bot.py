44bfc9d0efa1a8a7a27a97385ba2b6fcb5c9e7b7
Bastien Orivel <eijebong@bananium.fr>
2025-12-30 16:43:18 +0100

    Prevent the bot from pinging anyone except the right role and itself
    This shouldn't be any different since we sanitize messages anyway but
    since discord allows for filtering like this we might as well

--- botguette/bot.py
+++ botguette/bot.py
@@ -179,7 +179,7 @@ class ArchipelagoBot(discord.Client):
             timestamp=timestamp
         )
 
-        await interaction.edit_original_response(content=message, allowed_mentions=discord.AllowedMentions(roles=True))
+        await interaction.edit_original_response(content=message, allowed_mentions=discord.AllowedMentions(roles=[role], users=[interaction.user]))
         original_message = await interaction.original_response()
 
         if is_async:
